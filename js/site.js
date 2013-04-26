4d0d967e67d85a130f0d703af14966bdc36512ff
Nick Shvelidze <shveloo@gmail.com>
2013-04-26 21:32:03 +0400

    Update js

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ $(function() {
 	$('.platform').click(function() {
 		var platform = $(this).data('platform')
 
-		if(platform) {
+		if(platform && platform.length > 0) {
 			$('.post').not('[data-platform="' + platform + '"]').fadeOut('fast')
 			$('.post[data-platform="' + platform + '"]').fadeIn('fast')
 		} else {
