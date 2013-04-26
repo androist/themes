4da47487c1438564a49cdb8da26e54f68ebc7495
Nick Shvelidze <shveloo@gmail.com>
2013-04-26 21:55:23 +0400

    Allow multiple platforms

--- js/site.js
+++ js/site.js
@@ -20,8 +20,8 @@ $(function() {
 		var platform = $(this).data('platform')
 
 		if(platform && platform.length > 0) {
-			$('.post').not('[data-platform="' + platform + '"]').fadeOut('fast')
-			$('.post[data-platform="' + platform + '"]').fadeIn('fast')
+			$('.post').not('[data-platform~="' + platform + '"]').fadeOut('fast')
+			$('.post[data-platform=~"' + platform + '"]').fadeIn('fast')
 		} else {
 			$('.post').fadeIn('fast')
 		}
